3e4f2216385bd6d9ac0dbb15b655c8743db36969
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2022-06-01 11:07:18 +0100

    gitignore

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const __prod__ = process.env.Node_ENV === "production";
+export const dbConfig = {
+  dbName: "ShoutOut",
+  user: "admin",
+  password: "InfinityFarmery",
+};
+
+export const COOKIE_NAME = "urban:auth";
+export const FORGET_PASSWORD_PREFIX = "forget-password:";
