9ccdac36c84b98b2a31fb96b23a23a9ad2725149
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2022-06-10 15:37:40 +0100

    cookie name revised

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,4 @@
 export const __prod__ = process.env.Node_ENV === "production";
-export const dbConfig = {
-  dbName: "urbn",
-  user: "urbn",
-  password: "12345678",
-};
 
-export const COOKIE_NAME = "urban:auth";
-export const FORGET_PASSWORD_PREFIX = "forget-password:";
+export const COOKIE_NAME = "urbanauth";
+export const FORGET_PASSWORD_PREFIX = "forgot-password:";
